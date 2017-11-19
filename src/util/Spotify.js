e3d88cd691f5897d72ddf075687deebea16b9ddf
gmklinck <32417977+gmklinck@users.noreply.github.com>
2017-11-19 09:55:31 -0600

    update authorization

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,7 +25,7 @@ const Spotify = {
   search(term) {
     const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
-    { headers: {Authorization: 'Bearer ' + accessToken} }).then(response => {
+    { headers: {Authorization: `Bearer ${accessToken}`} }).then(response => {
       return response.json();
     }).then(jsonResponse => {
       if(!jsonResponse.tracks) {
@@ -46,7 +46,7 @@ const Spotify = {
       return;
     };
     const accessToken = Spotify.getAccessToken();
-    const headers = { Authorization: 'Bearer ' + accessToken};
+    const headers = { Authorization: `Bearer ${accessToken}`};
     let userId;
 
     return fetch('https://api.spotify.com/v1/me',
